ac8c8d2671e21b9e0949d7e5e58a585dcd9b5e32
ArhamSiddiqui1 <arhamkashif16@gmail.com>
2024-06-03 00:05:28 +0500

    marks calculator

--- main.dart
+++ main.dart
@@ -16,5 +16,14 @@ List hi =[
 ];
 print(hi[0]["name"][0]);
 print(hi[2]["hello"][3]["hi5"][0]);
+List new_inventory =[
+"sectors" , ["1", "2" ,{
+  "imploy" : {
+    "sallery" : [36 , 56 ,67 ,45 ,34 ,34 ,23 ,45]
+  }
+}
+]
 
+];
+print(new_inventory[1][2]["imploy"]["sallery"][6]);
 }
